b61ed412a0b0d9d3580d15153145ccb50b730edb
anthonom <150287038+anthonom@users.noreply.github.com>
2025-08-11 16:20:19 +0100

    Update main.py

--- tests/android/testhardware/main.py
+++ tests/android/testhardware/main.py
@@ -4,11 +4,11 @@ from jnius import autoclass
 print ('-- test hardware start!')
 
 Hardware = autoclass('org.renpy.android.Hardware')
-print 'DPI is', Hardware.getDPI()
+print ('DPI is', Hardware.getDPI())
 
 Hardware.accelerometerEnable(True)
 for x in xrange(20):
-    print Hardware.accelerometerReading()
+    print (Hardware.accelerometerReading())
     sleep(.1)
 
 print ('-- test hardware done!')
